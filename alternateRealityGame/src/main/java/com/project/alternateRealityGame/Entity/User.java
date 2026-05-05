7fe42a63e23865f3e7ac142c503020786b69c56b
iamRaz-01 <abdulrazak.nasriudeen@gmail.com>
2026-05-05 15:15:16 +0530

    Setup completed

---
+++ alternateRealityGame/src/main/java/com/project/alternateRealityGame/Entity/User.java
@@ -0,0 +1,4 @@
+package com.project.alternateRealityGame.Entity;
+
+public class User {
+}
---
+++ alternateRealityGame/src/test/java/com/project/alternateRealityGame/AlternateRealityGameApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.alternateRealityGame;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AlternateRealityGameApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
